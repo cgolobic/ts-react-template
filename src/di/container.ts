37cbcc71df71b3b8648f83df9a38d753e250504c
cgolobic <cgolobic@gmail.com>
2017-09-14 20:27:43 -0700

    wip: code organization and cleanup

--- src/di/container.ts
+++ src/di/container.ts
@@ -1,35 +1,23 @@
 import { ServiceRegistration } from './types/service-registration';
 import { FunctionRegistration } from './types/function-registration';
-var _registeredDependencies = {} as any;
 var _providerInstantiationComplete = false;
 var _providerMap: Map<string, any> = new Map();
 var _registrationMap: Map<string, ServiceRegistration> = new Map();
 var _instantiationStack: string[] = [];
 
-export function registerDependencyProviders (dependencyProviders: (ServiceRegistration | FunctionRegistration)[]) {
-  _providerMap = new Map();
-  _providerInstantiationComplete = false;
-  _instantiationStack = [];
-  dependencyProviders
-    .forEach((provider: ServiceRegistration | FunctionRegistration) => {
-      if (provider.hasOwnProperty('service')) {
-        let reg = provider as ServiceRegistration;
-        _registrationMap.set(reg.service.name, reg);
-      } else {
-        _registerFunction(provider as FunctionRegistration);
-      }
-    });
-  _registrationMap.forEach((val: ServiceRegistration) => _registerService(val));
-  _providerInstantiationComplete = true;
+// Internal functions
+
+function _isDependencyRegistered(key: string): boolean {
+  return _providerMap.has(key);
 }
 
 function _registerService(serviceRegistration: ServiceRegistration): void {
-  if (!isDependencyRegistered(serviceRegistration.service)) {
+  if (!_isDependencyRegistered(serviceRegistration.service.name)) {
     _instantiationStack.push(serviceRegistration.service.name);
     if (serviceRegistration.overrideService !== undefined && serviceRegistration.overrideService !== null) {      
-      _registeredDependencies[(serviceRegistration.service as any).name] = new serviceRegistration.overrideService();
+      _providerMap.set(serviceRegistration.service.name, new serviceRegistration.overrideService());
     } else {
-      _registeredDependencies[(serviceRegistration.service as any).name] = new serviceRegistration.service();
+      _providerMap.set(serviceRegistration.service.name, new serviceRegistration.service());
     }
     _instantiationStack.pop();
   }
@@ -39,38 +27,57 @@ function _registerFunction(functionRegistration: FunctionRegistration): void {
   if (functionRegistration.function.name === 'function') {
     throw Error('Registering anonymous functions is not permitted.');
   }
-  if (!isDependencyRegistered(functionRegistration.function)) {
+  if (!_isDependencyRegistered(functionRegistration.function.name)) {
     if (functionRegistration.overrideFunction !== undefined && functionRegistration.overrideFunction !== null) {
-      _registeredDependencies[functionRegistration.function.name] = functionRegistration.overrideFunction;
+      _providerMap.set(functionRegistration.function.name, functionRegistration.overrideFunction);
     } else {
-      _registeredDependencies[functionRegistration.function.name] = functionRegistration.function;
+      _providerMap.set(functionRegistration.function.name, functionRegistration.function)
     }
   }
 }
 
+
+// Exports
+
+export function registerDependencyProviders (dependencyProviders: (ServiceRegistration | FunctionRegistration)[]) {
+  _providerMap = new Map();
+  _providerInstantiationComplete = false;
+  _instantiationStack = [];
+
+  // Build a map of all registrations for reference when intantiating the dependency graph
+  dependencyProviders
+    .forEach((provider: ServiceRegistration | FunctionRegistration) => {
+      if (provider.hasOwnProperty('service')) {
+        let reg = provider as ServiceRegistration;
+        _registrationMap.set(reg.service.name, reg);
+      } else {
+        _registerFunction(provider as FunctionRegistration);
+      }
+    });
+
+  // Recursively instantiate and register all service providers in the dependency graph
+  _registrationMap.forEach((serviceRegistration: ServiceRegistration) => _registerService(serviceRegistration));
+
+  _providerInstantiationComplete = true;
+}
+
  export function fetchDependency(key: string) {
-  let registeredDependency = _registeredDependencies[key];
   if (!_providerInstantiationComplete) {
-    if (_registrationMap.get(key) === undefined) {
+    if (!_registrationMap.has(key)) {
       throw Error(`Dependency ${key} has not been registered with the dependency container.`);
     }
-    if (registeredDependency === undefined || registeredDependency === null) {
+    if (!_isDependencyRegistered(key)) {
       let registration = _registrationMap.get(key);
       if (_instantiationStack.find((providerKey: string) => providerKey === key)) {
         throw Error('Circular dependency detected');
       }
       _registerService(registration);
     }
-    return _registeredDependencies[key];
   } else {
-    if (registeredDependency === undefined || registeredDependency === null) {
+    if (!_isDependencyRegistered(key)) {
       throw new Error(`Dependency ${key} has not been registered with the dependency container.`);
     }
-    return registeredDependency;
   }
-}
-
-function isDependencyRegistered(dependency: any): boolean {
-  return _registeredDependencies.hasOwnProperty(dependency.name);
+  return _providerMap.get(key);
 }
   
